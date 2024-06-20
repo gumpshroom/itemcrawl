4143958a01de22ccd5ac4020b9ef91b6595c6011
Yang <eric.yang.ext@mbs-cc.com>
2024-06-20 15:48:14 -0400

    started ggames bot

---
+++ scripts/chatbot.js
@@ -0,0 +1,51 @@
+Object.assign(globalThis, require("kolmafia"));
+
+var ticketList = ["small box", "large box", "jumping horseradish", "perfect cosmopolitan", "perfect dark and stormy", "perfect mimosa", "perfect negroni", "perfect old-fashioned", "perfect paloma", "Sacramento wine", "hacked gibson", "red pixel potion", "octolus oculus", "spooky hi mein", "stinky hi mein", "hot hi mein", "cold hi mein", "sleazy hi mein", "zombie", "elemental caipiroska", "perfect ice cube", "golden gum", "snow berries", "Game Grid ticket", "scrumptious reagent", "milk of magnesium", "tiny bottle of absinthe", "Bloody Nora", "llama lama gong", "van key", "tattered scrap of paper", "ice harvest"]
+var runningGame = false
+function main(sender, message) {
+    var args = message.split(" ");
+    var cmd = args[0];
+    args = args.slice(1);
+    switch (cmd) {
+        case "help":
+            chatPrivate(sender, "help me add this help message")
+            break;
+        case "host":
+            var prize = parseInt(args[1])
+            if (prize && prize > 10000 && prize < 200000 && myMeat() >= prize) {
+                var foundItem = false;
+                var item;
+                while (!foundItem) {
+                    item = Item.get(ticketList[Math.floor(Math.random() * ticketList.length)])
+                    if (itemAmount(item) >= 10) {
+                        foundItem = true;
+                    }
+                }
+                refreshShop();
+                if (!runningGame && Object.keys(getShop()).length === 0) {
+                    putShop(100, 1, 10, item);
+                    runningGame = true;
+                    chatGames("AR requested by " + sender + " with prize 1d" + prize + " meat !")
+                    while (runningGame) {
+                        refreshShop();
+                        if (Object.keys(getShop()).length === 0) {
+                            runningGame = false;
+
+                        }
+                    }
+                }
+            } else {
+                chatPrivate(sender, "i dont have enough meat or the prize amount is invalid")
+            }
+            break;
+        case "roll":
+            break;
+    }
+}
+function chatGames(msg) {
+    visitUrl("submitnewchat.php?playerid=" + myId() + "&pwd=" + myHash() + "&graf=" + msg + "&j=1");
+}
+function getTicketHolders() {
+    var shop = getShopLog();
+    
+}
---
+++ scripts/ggames.js
@@ -0,0 +1,5 @@
+Object.assign(globalThis, require("kolmafia"));
+chatbotScript = require("chatbot.js");
+
+
+
