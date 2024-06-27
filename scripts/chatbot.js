862951adafc40192609d28a580cec9b11b190c43
Yang <eric.yang.ext@mbs-cc.com>
2024-06-27 14:02:34 -0400

    more info on win

--- scripts/chatbot.js
+++ scripts/chatbot.js
@@ -81,10 +81,16 @@ function main(sender, message) {
                             var shopLog = getShopLog()
                             //print(shopInv)
                             var winnerIndex = Math.floor(Math.random() * gameSize) + 1
-                            var winner = shopLog[winnerIndex - 1].match(/ \d\d:\d\d:\d\d (.*) bought/)[1]
+                            var match = shopLog[winnerIndex - 1].match(/ \d\d:\d\d:\d\d (.*) bought (\d*) \((.*)\)/)
+                            var winner = match[2]
+                            var boughtTime = match[1]
+                            var ticketName = match[3]
                             var amount = Math.floor(Math.random() * prize) + 1
-
-                            chatGames("game ended !! rolling 1d" + gameSize + " gives " +  + winnerIndex + ".. " + winner + " won " + amount + " meat. " + Math.random() > 0.5 ? "congrats!!" : "(._.)-b")
+                            var msg = "game ended !! rolling 1d" + gameSize + " gives " +  (gameSize - winnerIndex) + "..."
+                            Waitq(5)
+                            msg = winner + " bought a " + ticketName + " at " + match[1] + " and won " + amount + " meat. "
+                            msg += Math.random() > 0.5 ? "congrats!!" : "(._.)-b"
+                            chatGames(msg)
                             
                             //kmail
 
@@ -123,9 +129,11 @@ function main(sender, message) {
                 var roll = parseInt(args[0].split("d")[1].replace("k", "000").replace("m", "000000"))
                 var result = Math.floor(Math.random() * roll) + 1
                 if(message.includes("in games")) {
-                    chatGames(sender + " rolled " + result + " out of " + roll + Math.random() > 0.5 ? ". (._.)-b" : ".")
+                    var msg = sender + " rolled " + result + " out of " + roll
+                    msg += Math.random() > 0.5 ? ". (._.)-b" : "."
+                    chatGames(msg)
                 } else {
-                    chatPrivate(sender, "you rolled " + result + " out of " + roll)
+                    chatPrivate(sender, "you rolled " + result + " out of " + roll + ".")
                 }
                 
             } else {
