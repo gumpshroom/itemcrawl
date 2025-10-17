2b8e891d57cdd959d3491b5a104d2163ad71367f
gumpshroom <43548339+gumpshroom@users.noreply.github.com>
2025-10-17 05:10:25 -0400

    Update chatbot.js

--- scripts/chatbot.js
+++ scripts/chatbot.js
@@ -292,7 +292,7 @@ function main(sender, message) {
             
             // Check if bot has enough total meat (jackpot + prize + buffer)
             var totals = calculateTotalAllocations();
-            var botHasEnoughMeat = (myMeat() - globalObj.jackpot - totals.totalUserAllocations) + 50 >= prize;
+            var botHasEnoughMeat = (myMeat() - globalObj.jackpot) + 50 >= prize;
             if (!botHasEnoughMeat) {
                chatPrivate(sender, "i dont have enough meat or the prize amount is invalid. (i have " + numberWithCommas(myMeat()) + " meat)");
                break;
